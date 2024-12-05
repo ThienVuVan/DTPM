af0a98dff0ea595cf32ab60c8b2cd7034a278085
Hoàng Nguyên <110187720+hoangnguyenhtng@users.noreply.github.com>
2024-12-05 21:34:09 +0700

    ahfdgl

--- src/main/java/com/bookstore/entity/ChiTietDon.java
+++ src/main/java/com/bookstore/entity/ChiTietDon.java
@@ -17,6 +17,9 @@ public class ChiTietDon {
     @JoinColumn(name = "order_id", referencedColumnName = "order_id")
     private Don order;
 
+    @ManyToOne
+    @JoinColumn(name = "book_id", referencedColumnName = "book_id")
+    private Sach sach;
+
     private Integer quantity;
-    private Double price;
 }
